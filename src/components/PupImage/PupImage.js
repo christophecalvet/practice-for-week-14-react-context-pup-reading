cf70f4beb55aed0516b3772dac737c96b650e7c8
Christophe Calvet <onlyatest@mytest.com>
2024-09-18 14:14:58 +0200

    maybe working

--- src/components/PupImage/PupImage.js
+++ src/components/PupImage/PupImage.js
@@ -1,10 +1,16 @@
+// ./src/components/PupImage/PupImage.js
+import { useContext } from 'react';
+import { PupContext } from '../../context/PupContext';
 import speedy from '../../pups/speedy-pup.jpg';
 import banana from '../../pups/banana-pup.jpg';
 import sleepy from '../../pups/sleepy-pup.jpg';
+import { usePuppyType } from '../../context/PupContext'
 
 const PupImage = () => {
+  const { puppyType } = useContext(PupContext);
+
   return (
-    <img src={speedy} alt="pup" />
+    <img src={puppyType} alt="pup" />
   );
 };
 
